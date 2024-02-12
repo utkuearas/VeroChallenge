a42d295c161f0ce155017c18ce259fb00c073274
Utku Erdoğanaras <utkuearasbusiness@gmail.com>
2024-02-12 06:54:39 +0300

    Client file is implemented

--- client.py
+++ client.py
@@ -1,3 +1,57 @@
+import argparse
+import pandas as pd
+import json
+import requests
+from datetime import datetime, timedelta
+import numpy as np
+parser = argparse.ArgumentParser()
+parser.add_argument('-k','--keys',default='')
+parser.add_argument('-c','--colored',default='True')
+parser.add_argument('-f','--file',default='./vehicles.csv')
 
 
-# TODO Create POST call function
+def labelMatch(row):
+    labels = [''] * len(row)
+    if not row.isnull()['colorCode']:
+        index = row.index.get_loc('labelIds')
+        color = row['colorCode']
+        labels[index] = f'color: {color}'
+    return labels
+
+def styleRow(row):
+    date = datetime.strptime(row['hu'],'%Y-%m-%d')
+    now = datetime.now()
+    if now - timedelta(days=90) < date:
+        return ['background-color: green'] * len(row)
+    elif now - timedelta(days=30*12) < date:
+        return ['background-color: orange'] * len(row)
+    else:
+        return ['background-color: red'] * len(row)
+
+if __name__ == '__main__':
+
+    args = parser.parse_args()
+    print(args)
+    
+    keys = args.keys
+    colored = args.colored
+    if colored == True:
+        colored = True
+    else:
+        colored = False
+    csv_file_name = args.file
+
+    files = {'file': (csv_file_name,open(csv_file_name, 'rb'),'text/csv')}
+    res = requests.post('http://localhost:8000/uploadCSV', files=files)
+    
+    data = json.loads(res.text)
+    data = pd.read_json(data)
+    
+    #data = data[data['rnr'].notna()]
+    data = data.sort_values('gruppe')
+
+    if colored:
+        data = data.style.apply(styleRow,axis=1).apply(labelMatch,axis=1)
+    else:
+        data = data.style.apply(labelMatch,axis=1)
+    data.to_excel('./result.xlsx')
--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, UploadFile, File
 from io import BytesIO
-import os
+import numpy as np
 import pandas as pd
 import requests
+import aiohttp
 from pprint import pprint
 import json
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+import os
+import asyncio
 load_dotenv()
 
 app = FastAPI()
@@ -18,15 +21,35 @@ CURRENT_TOKEN = {'token':None,
 DATA_URI = os.getenv('DATA_URI')
 LOGIN_URI = os.getenv('LOGIN_URI') 
 TOKEN = os.getenv('TOKEN')
-USERNAME = os.getenv('USERNAME')
+USERNAME = os.getenv('LOGIN_USERNAME')
 PASSWORD = os.getenv('PASSWORD')
+LABEL_ID = os.getenv('LABEL_ID')
 
-async def request_data(token):
+async def request_data(token, URI, session=None):
 
     headers = {'Authorization': 'Bearer '+ token}
-    data = requests.get(DATA_URI, headers=headers)
+    if session is not None:
+        data = await session.get(URI, headers=headers)
+        return json.loads(await data.text())
+    
+    data = requests.get(URI, headers=headers)
     return json.loads(data.text)
 
+
+def merge_two_column(cols):
+    col1,col2 = cols
+    null1,null2 = cols.isnull()
+    if not null1 and null2:
+        return col1
+    elif not null2 and null1:
+        return col2
+    elif null2 == null1:
+        if not null1:
+            return col1
+        else:
+            return ""
+    else:
+        return str(col1) + "," + str(col2)
 
 async def get_token():
 
@@ -69,16 +92,67 @@ async def uploadCSV(file: UploadFile=File(...)):
     buffer = BytesIO(content)
     
     # Creating pandas dataframe object
-    df = pd.read_csv(buffer, sep=';')
+    client_request = pd.read_csv(buffer, sep=';')
+    client_request.to_excel('./client.xlsx')
 
     token = await get_token()
-    data = await request_data(token)
+    data = await request_data(token,DATA_URI)
 
-    # Filter data
-    filteredData = [i for i in data if i['hu']]
-    print(filteredData)
-    filteredData = [i for i in filteredData if i['labelIds']]
-    print(filteredData)
-    return {"Success"}
+    api_response = pd.DataFrame.from_records(data)
+    api_response.to_excel('./api.xlsx')
 
-    # TODO Modify this function
+    api_columns = set(api_response.columns)
+    client_columns = set(client_request.columns)
+
+    if len(api_columns) > len(client_columns):
+        large = api_response
+        small = client_request
+    else:
+        large = client_request
+        small = api_response
+    
+    large['kurzname'] = large['kurzname'].astype(str)
+    small['kurzname'] = small['kurzname'].astype(str)
+    large = large.merge(small, on=['kurzname','gruppe','lagerort'], how='outer')
+    large = large.fillna(np.nan)
+    large = large.replace('NULL',np.nan)
+
+    cols = [i for i in large.columns if i.endswith('_x')]
+    for col in cols:
+        col_name = col[:len(col)-2]
+        large[col_name] = large[[col_name+'_x',col_name+'_y']].apply(merge_two_column, axis=1)
+        large = large.drop([col_name+'_x',col_name+'_y'], axis=1)
+
+    large = large.drop_duplicates()
+
+    labels = set()
+
+    for _, i in large.iterrows():
+        if i['labelIds']:
+           labels.add(str(int(float(str(i['labelIds']).split(',')[0]))))
+
+    #large = large[large['hu'].notna()]
+
+    color_codes = dict()
+    async with aiohttp.ClientSession() as session:
+        labels_f = [asyncio.create_task(request_data(token,LABEL_ID+uri,session=session)) for uri in list(labels)]
+        responses = await asyncio.gather(*labels_f)
+        for res in responses:
+            color_code = res[0]['colorCode']
+            if color_code is not '':
+                color_codes[res[0]['id']] = color_code
+
+    large['colorCode'] = pd.DataFrame([np.nan]*len(large))
+    
+    for i in range(len(large)):
+        try:
+            label = int(float(large.iloc[i]['labelIds']))
+        except:
+            continue
+        if label in color_codes:
+            large.loc[i,'colorCode'] = color_codes[label]
+    #large = large.replace('',np.nan)
+
+    result = large.to_json(orient='records')
+    
+    return result
